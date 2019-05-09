3966e316cf9fe78ecb0489296c8bbda4dcecff0d
att4ck3rs3cur1ty <abimaelima54@gmail.com>
2019-05-09 00:10:24 -0300

    adicionando header do ave.h

---
+++ ave.h
@@ -0,0 +1,13 @@
+#ifndef AVE_H
+#define AVE_H
+
+class Ave : public Animal{
+	protected:
+		double m_tamanho_do_bico_cm;
+		double m_envergadura_das_asas;
+	public:
+		Ave();
+		~Ave();
+};	
+
+#endif
