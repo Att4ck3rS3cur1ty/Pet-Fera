2a217348cc03747a0e6683312fb6f5515bd58c3d
att4ck3rs3cur1ty <abimaelima54@gmail.com>
2019-05-08 23:51:06 -0300

    adicionando o básico do veterinario.h

---
+++ veterinario.h
@@ -0,0 +1,13 @@
+#ifndef VETERINARIO_H
+#define VETERINARIO_H
+#include "funcionario.h"
+
+class Veterinario{
+	private:
+		private std::string m_crmv;
+	public:
+		Veterinario();
+		~Veterinario();
+};
+
+#endif
