880a8086b22417a9ef5a276e680a4fc6e7b6031a
guilhermelm17 <guilherme01lira@gmail.com>
2019-05-17 20:40:12 -0300

    comentando a declaração da classe mamifero

--- header/mamifero.h
+++ header/mamifero.h
@@ -12,10 +12,13 @@ class mamifero : public Animal {
 		Mamifero(int id, std::string nome, std::string nome_cientifico, char sexo,float tamanho, std::string dieta, Veterinario veterinario, Tratador tratador, std::string nome_batismo, std::string cor_pelo);
 		~Mamifero();
 	
+	//Preencher a classe mamifero
 	bool preencher_atributos_classe();
 	
+	//Acessar o atribulo da cor do pelo
 	std::string getCorPelo();
-	
+
+	//Alterar o atributo da cor do pelo	
 	void setCorPelo(cor_pelo);
 
 #endif
