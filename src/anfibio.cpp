a0564f628aa5dab924073d7111e48c5db933413b
guilhermelm17 <guilherme01lira@gmail.com>
2019-05-17 20:20:09 -0300

    mudanças na implementação de anfibio

--- src/anfibio.cpp
+++ src/anfibio.cpp
@@ -2,7 +2,7 @@
 #include "../include/animal.h"
 #include <iostream>
 
-	Anfibio::Anfibio(){};
+	Anfibio::Anfibio(){}
 	Anfibio::Anfibio(int id,std::string nome, std::string nome_cientifico, std::string classe, char sexo, float tamanho, std::string dieta, std::string nome_batismo, Veterinario veterinario, Tratadir tratador, int total_mudas, std::string ultima_muda):Animal(m_id, m_nome, m_nome_cientifico, m_classe, m_sexo, m_tamanho, m_dieta, m_nome_batismo, m_veterinario, m_tratador), m_total_mudas(m_total_mudas), ultima_muda(ultima_muda){}
 	Anfibio::~Anfibio(){}
 	
@@ -13,19 +13,19 @@
 	
 	//Acessar o numero total de mudas
 	int Anfibio::getTotalMudas(){
-		return this->m_total_mudas;
+		return m_total_mudas;
 }
 
 	//Acessar a ultima muda
 	std::string Anfibio::getUltimaMuda(){
-		return this->m_ultima_muda;
+		return m_ultima_muda;
 }
 	
 	//Alterar o total de mudas
 	void Anfibio::setTotalMudas(int total_mudas){
-		this->total_mudas = m_total_mudas;
+		m_total_mudas = total_mudas;
 }
 	//Alterar a ultima muda
 	void Anfibio::setUltimaMuda(std::string ultima_muda){
-		this->m_ultima_muda = ultima_muda;
+		m_ultima_muda = ultima_muda;
 }
